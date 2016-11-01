086c2ae3dff67f595452887672de6f1cdc0f70e2
wwyao <708919996@qq.com>
2016-11-01 08:42:36 +0800

    buffer,attribute

---
+++ animation.js
@@ -0,0 +1,78 @@
+/*
+*匀速运动
+*ele：元素
+*target:目标距离
+*direction：运动的方向，取值：'v','h'（默认）
+*velocity：运动速度
+*/
+function uniformMove(ele,target,direction,velocity){
+	direction = direction || 'h';
+	clearInterval(ele.timeId);
+	ele.timeId = setInterval(function(){
+		var offset = (direction === 'v')?ele.offsetTop:ele.offsetLeft;
+		var dir = (direction === 'v')?'top':'left';
+		var speed = target > offset?velocity:-velocity;
+		if(offset === target) {
+				clearInterval(ele.timeId);
+				ele.style[dir] = target + "px";
+			}else {
+				ele.style[dir] = offset + speed + "px";
+			}
+	},20);
+}
+
+
+/*
+*缓冲运动
+*ele：元素
+*target:目标距离
+*direction：运动的方向，取值：'v','h'（默认）
+*velocity：运动速度，值越小速度越快，值越大速度越慢
+*/
+function bufferMove(ele,target,direction,velocity){
+	direction = direction || 'h';
+	clearInterval(ele.timeId);
+	var speed;
+	ele.timeId = setInterval(function(){
+		var offset = (direction === 'v')?ele.offsetTop:ele.offsetLeft;
+		var dir = (direction === 'v')?'top':'left';
+		speed = (target - offset)/velocity;
+		speed = speed > 0?Math.ceil(speed):Math.floor(speed);
+		if(offset === target) {
+			clearInterval(ele.timeId);
+		}else {
+			ele.style[dir] = offset + speed + "px";
+		}	
+	},20);
+}
+
+
+
+
+/*
+*属性变化过程的动画
+*obj：目标元素
+*target：属性的目标值
+*att：要改变的属性名
+*/
+function attributeAnim(obj,att,target) {
+	clearInterval(obj.timeId);
+	obj.timeId = setInterval(function(){
+		var start = (att === 'opacity')?parseInt(parseFloat(getStyle(obj,att))*100):parseInt(getStyle(obj,att));
+		var speed = (target - start)/7
+		speed = speed > 0?Math.ceil(speed):Math.floor(speed);
+		if(start === target) {
+			clearInterval(obj.timeId);
+		} else {
+			obj.style[att]  = (att === 'opacity')?(start + speed)/100:(start + speed) + "px";
+		}
+	},30);
+}
+//获取某元素的属性(该元素可以是外部，内部，内嵌)
+function getStyle(obj,att) {
+	if(obj.currentStyle){
+		return obj.currentStyle[att];
+	} else {
+		return getComputedStyle(obj)[att];
+	}
+}
